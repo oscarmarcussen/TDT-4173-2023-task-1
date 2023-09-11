4220791b99b4b67b03b6fc921846936a990b07e8
Oscar Timm Marcussen <oscar.marcussen@aboveit.no>
2023-09-11 21:29:26 +0200

    start on decission trees

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -1,12 +1,13 @@
 import numpy as np 
-import pandas as pd 
+import pandas as pd
+import array
 # IMPORTANT: DO NOT USE ANY OTHER 3RD PARTY PACKAGES
 # (math, random, collections, functools, etc. are perfectly fine)
 
 
 class DecisionTree:
     
-    def __init__():
+    def __init__(self):
         # NOTE: Feel free add any hyperparameters 
         # (with defaults) as you see fit
         pass
@@ -21,9 +22,66 @@ class DecisionTree:
                 to the features.
             y (pd.Series): a vector of discrete ground-truth labels
         """
-        # TODO: Implement 
-        raise NotImplementedError()
-    
+        # TODO: Implement
+
+        """
+        Calculating total entropy
+        """
+        tot_entropy_arr = y.value_counts().to_numpy(dtype=int)
+        tot_entropy = entropy(tot_entropy_arr)
+        print(tot_entropy)
+
+        """
+        Calculating entropy for each feature
+        """
+
+        data = X.merge(y.to_frame(), left_index=True, right_index=True)
+        attr_names = X.columns.to_list()
+        items_data = []
+
+        for n in attr_names:
+            values = X[n].unique()
+            for v in values:
+                filtered_data = data[data[n] == v]
+                entropy_arr = filtered_data[y.name].value_counts().to_numpy(dtype=int)
+                value_entropy = entropy(entropy_arr)
+                item = {'Label': n, 'Value': v, 'Entropy': value_entropy}
+                items_data.append(item)
+
+        entropy_df = pd.DataFrame(items_data) # df with the entropy of the different values of the different attributes
+        print(entropy_df.head())
+
+        """
+        Calculating entropy for each feature
+        
+        feature_value_data: df containing data with a specific value of a feature (eg. Outlook = Sunny)
+        label: string, name of the label of the df (=PlayTennis)
+        class_list: list, unique classes of the label (=[Yes, No])
+        
+        returns float, calculated entropy of the feature value df (eg. for Outlook = Sunny returns 0.971)
+        """
+        """
+        def calc_entropy(feature_value_data, label, class_list):
+            class_count = feature_value_data.shape[0]
+            entropy = 0
+
+            for c in class_list:
+                label_class_count = feature_value_data[feature_value_data[label] == c].shape[0]
+                entropy_class = 0
+                if label_class_count != 0:
+                    probability_class = label_class_count/class_count #probability of the class
+                    entropy_class = - probability_class * np.log2(probability_class) #entropy
+                entropy += entropy_class
+            return entropy
+
+        df = X.merge(y.to_frame(), left_index=True, right_index=True)
+
+        print(df.head())
+        """
+
+
+        #raise NotImplementedError()
+
     def predict(self, X):
         """
         Generates predictions
@@ -85,7 +143,7 @@ def entropy(counts):
     Computes the entropy of a partitioning
     
     Args:
-        counts (array<k>): a lenth k int array >= 0. For instance,
+        counts (array<k>): a length k int array >= 0. For instance,
             an array [3, 4, 1] implies that you have a total of 8
             datapoints where 3 are in the first group, 4 in the second,
             and 1 one in the last. This will result in entropy > 0.
